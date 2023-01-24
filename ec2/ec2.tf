3738a7af52c2b044173b0e2fdf8e15abb76e66a2
shomoj <111367225+shomoj@users.noreply.github.com>
2023-01-23 22:33:18 -0600

    Create ec2.tf

---
+++ ec2/ec2.tf
@@ -0,0 +1,11 @@
+#EC2 instance -resource block
+resource "aws_instance" "proj1" {
+  ami           = "ami-0b5eea76982371e91" # us-east-1
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.privatesubnet
+  key_name   = "key29"
+  
+  tags = {
+    "Name" = "project1"
+  }
+}
