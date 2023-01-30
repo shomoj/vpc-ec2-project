0144622524265b1069717d82a38bc60b74399810
shomoj <111367225+shomoj@users.noreply.github.com>
2023-01-29 21:43:41 -0600

    Update ec2variable.tf

--- ec2/ec2variable.tf
+++ ec2/ec2variable.tf
@@ -1,55 +1,25 @@
-# AWS EC2 Instance Terraform Variables
-# EC2 Instance Variables
-
-# Ec2 instance region
-variable "region" {
-  type        = string
-  description = "the instance region"
-  #default     = "us-east-1"
-}
-
-#AWS Instance AMI
 variable "ami" {
   type        = string
   description = "the ami id"
-  default     = "ami-0b5eea76982371e91"
+  #default     = "ami-0b5eea76982371e91"
 }
 
-#AWS instance type
 variable "instance_type" {
   type        = string
   description = "the instance type"
-  default     = "t2.micro"
+  #default     = "t2.micro"
 }
 
-
-#AWS instance key
-variable "key_name" {
-  type = string
-  description = "the instance key"
-  default = "key29"
-}
-
-
-#AWS ec2-tag
 variable "ec2-tag" {
   type        = string
   description = "the instance tag"
-  default     = "project1"
+  #default     = "project1"
 }
 
-#AWS privatesubnetid
-variable "privatesubnetid" {
+
+variable "key_name" {
   type        = string
-  description = "the instance private subnet"
-  default     = "aws_subnet.privatesubnet"
+  description = "the instance key"
+  #default = "key29"
+
 }
-
-#AWS publicsubnetid
-variable "publicsubnetid" {
-  type        = string
-  description = "the instance public subnet"
-  #default     = "aws_subnet.publicsubnet"
-}
-
-
