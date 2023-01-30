3ffe5784f94c183909ae593f9ed05083d5c2e677
shomoj <111367225+shomoj@users.noreply.github.com>
2023-01-29 21:41:45 -0600

    Update vpc-variable.tf

--- vpc module/vpc-variable.tf
+++ vpc module/vpc-variable.tf
@@ -1,4 +1,3 @@
-
 variable "region" {
   type        = string
   description = "the instance region"
@@ -6,50 +5,53 @@ variable "region" {
 }
 
 #VPC cider block
-variable "vpc_cidr" {
-type = string
-description = "vpc cidr block"
-default = "10.0.0.0/16"
+variable "cidr_block" {
+  type        = string
+  description = "vpc cidr block"
+  default     = "10.0.0.0/16"
 }
 
-# VPC Availability Zones
-variable "vpc_availability_zones" {
-  description = "VPC Availability Zones"
-  type = list(string)
-  default = ["us-east-1a", "us-east-1b"]
-}
 
 #VPC tag
 variable "vpc-tag" {
   type        = string
   description = "the vpc tag"
-  #default     = "myfirstvpc"
+  default     = "myfirstvpc"
 }
 
 # VPC Public Subnet
 variable "public_subnet_cidr" {
-type = string
-description = "public subnet cidr block"
-default = "10.0.101.0/24"
+  type        = string
+  description = "public subnet cidr block"
+  default     = "10.0.101.0/24"
 }
 
-# VPC Public Subnet tag
+variable "public_subnet_az" {
+  type        = string
+  description = "public subnet az"
+  default     = "us-east-1a"
+}
+
 variable "pu-subnet-tag" {
   type        = string
   description = "public-subnet tag"
-  #default     = "publicsubnet"
+  default     = "publicsubnet"
 }
 
-# VPC Private Subnet 
-variable "private_subnet_cidr" {
-type = string
-description = "private subnet cidr block"
-default = "10.0.1.0/24"
+
+variable "pr_subnet_cidr" {
+  type        = string
+  description = "private subnet cidr block"
+  default     = "10.0.1.0/24"
 }
 
-# VPC Private Subnet tag
+variable "private_subnet_az" {
+  type        = string
+  description = "private subnet az"
+  default     = "us-east-1b"
+}
 variable "pr-subnet-tag" {
   type        = string
   description = "private subnet tag"
-  #default     = "privatesubnet"
+  default     = "privatesubnet"
 }
